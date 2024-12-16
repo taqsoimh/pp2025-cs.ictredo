cf4204076dab42af268d44986099586fb1918581
tAQ <132645387+taqsoimh@users.noreply.github.com>
2024-12-16 15:25:24 +0700

    Update: Change it to make program more "OOP"

--- 2.student.mark.oop.py
+++ 2.student.mark.oop.py
@@ -5,58 +5,32 @@ class Student:
         self.name = name
         self.dob = dob
         self.mark = {}
-    
+    def __str__(self):
+        return f"ID: {self.id} - Name: {self.name} - DoB: {self.dob}"
 class Course:
     def __init__(self, id: str, name: str):
         self.id = id
         self.name = name
+    def __str__(self):
+        return f"ID: {self.id} - Name: {self.name}"
 
-list_student = [] #list
-list_course = [] #list
 
-def add_mark():
-    print_all_courses()
-    c_id = input("Input Course ID:")
-    for course in list_course:
-        if(course.id == c_id):
-            print_all_students()
-            st_id = input("Input Student ID that you want to add mark: ")
-            for st in list_student:
-                if(st.id == st_id):
-                    st.mark[c_id] = input("Input mark: ")
-def input_student():
-    st_id = input("Enter student ID: ")
-    name = input("Enter student name: ")
-    dob = input("Enter student Date of Birth (YYYY-MM-DD): ")
-    student = Student(st_id, name, dob)
-    list_student.append(student)
-
-def input_course():
-    c_id = input("Enter course ID: ")
-    c_name = input("Enter course name: ")
-    course = Course(c_id, c_name)
-    list_course.append(course)
-
-def print_all_courses():
-    print("Courses:")
-    for course in list_course:
-        print(f"{course.id}: {course.name}")
-    print("-"*5)
-
-def print_all_students():
-    print("Student:")
-    for st in list_student:
-        print(f"{st.id} - {st.name} - {st.dob}")
-    print("-"*5)
-
-def print_mark():
-    print_all_courses();
-    c_id = input("Enter course ID to view marks: ")
-    print(f"Marks for course {c_id}:")
-    for st in list_student:
-        marks = st.mark.get(c_id, 'No marks entered')
-        print(f"{st.name}: {marks}")
-
+class Student_list:
+    def __init__(self):
+        self.students = []
+    def add_student(self, student):
+        self.students.append(student)
+    def print_all_student(self):
+        for st in self.students:
+            print(st)
+class Course_list:
+    def __init__(self):
+        self.courses = []
+    def add_course(self, course):
+        self.courses.append(course)
+    def print_all_course(self):
+        for c in self.courses:
+            print(c)
 
 dialogue_str = """
 Menu:
@@ -72,6 +46,8 @@ Select an option:
 """
 
 def main():
+    list_student = Student_list()
+    list_course = Course_list()
     while True:
         choice = int(input(dialogue_str))
         if choice not in [0,1,2,3,4,5,6,7]:
@@ -80,19 +56,40 @@ def main():
         if choice == 0:
             os.system('cls' if os.name == 'nt' else 'clear') # Window: cls != Linux: clear
         elif choice == 1:
-            input_student()
+            st_id = input("Enter student ID: ")
+            name = input("Enter student name: ")
+            dob = input("Enter student Date of Birth (YYYY-MM-DD): ")
+            student = Student(st_id, name, dob)
+            list_student.add_student(student)
         elif choice == 2:
-            input_course()
+            c_id = input("Enter course ID: ")
+            c_name = input("Enter course name: ")
+            course = Course(c_id, c_name)
+            list_course.add_course(course)
         elif choice == 3:
-            add_mark()
+            c_id = input("Enter course ID to view marks: ")
+            print(f"Marks for course {c_id}:")
+            for c in list_course.courses:
+                if(c.id == c_id):
+                    list_student.print_all_student()
+                    st_id = input("Input Student ID that you want to add mark: ")
+                    for st in list_student.students:
+                        if(st.id == st_id):
+                            inp_mark = input("Input mark: ")
+                            st.mark[c_id] = float(inp_mark)
         elif choice == 4:
-            print_all_students()
+            list_student.print_all_student()
         elif choice == 5:
-            print_all_courses()
+            list_course.print_all_course()
         elif choice == 6:
-            print_mark()
-        elif choice == 7:
-            print("Bai bai <3 <3 hihiihhihihiihhihihhhiihihihi")
+            list_course.print_all_course();
+            c_id = input("Enter course ID to view marks: ")
+            print(f"Marks for course {c_id}:")
+            for st in list_student.students:
+                marks = st.mark.get(c_id, 'No marks entered')
+                print(f"{st.name}: {marks}")
+        elif choice == 1337:
+            print("GoodBye!!!")
             break
 
 if __name__ == "__main__":
